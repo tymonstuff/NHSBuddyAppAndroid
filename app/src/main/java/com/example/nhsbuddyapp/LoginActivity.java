a283a6046d811001d2b534341a01c391d2e8d812
Tymon Rybak <tymonrybak@outlook.com>
2019-02-16 17:52:26 +0000

    Update views and project structure

--- app/src/main/java/com/example/nhsbuddyapp/LoginActivity.java
+++ app/src/main/java/com/example/nhsbuddyapp/LoginActivity.java
@@ -1,4 +1,90 @@
-package PACKAGE_NAME;
+package com.example.nhsbuddyapp;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity
+{
+    private EditText m_email, m_password;
+    private Button m_login_btn;
+    private TextView m_register_btn;
+
+    private FirebaseAuth m_auth;
+
+    //TODO: Attempt to invoke virtual method 'com.google.android.gms.tasks.Task com.google.firebase.auth.FirebaseAuth.createUserWithEmailAndPassword(java.lang.String, java.lang.String)' on a null object reference
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        m_auth = FirebaseAuth.getInstance();
+
+        m_email = findViewById(R.id.email_field);
+        m_password = findViewById(R.id.password_field);
+        m_login_btn = findViewById(R.id.login_btn);
+        m_register_btn = findViewById(R.id.sign_in_btn);
+
+    }
+
+    public void onLoginClick(View view)
+    {
+        String email_txt = m_email.getText().toString();
+        String password_txt = m_password.getText().toString();
+
+        // Ensure that fields are not empty on click
+        if(TextUtils.isEmpty(email_txt) || TextUtils.isEmpty(password_txt))
+        {
+            Toast.makeText(LoginActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            m_auth.signInWithEmailAndPassword(email_txt, password_txt)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void onRegisterLinkClick(View view)
+    {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+
+
 }
--- app/src/main/java/com/example/nhsbuddyapp/RegisterActivity.java
+++ app/src/main/java/com/example/nhsbuddyapp/RegisterActivity.java
@@ -1,4 +1,123 @@
 package com.example.nhsbuddyapp;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity
+{
+    private EditText m_username, m_email, m_password;
+    private Button m_register_btn;
+
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        m_username = findViewById(R.id.username_field);
+        m_email = findViewById(R.id.email_field);
+        m_password = findViewById(R.id.password_field);
+        m_register_btn = findViewById(R.id.register_btn);
+
+    }
+
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    public void onRegisterClick(View view)
+    {
+        String username_txt = m_username.getText().toString();
+        String email_txt = m_email.getText().toString();
+        String password_txt = m_password.getText().toString();
+
+        if(TextUtils.isEmpty(username_txt) ||TextUtils.isEmpty(email_txt) ||TextUtils.isEmpty(password_txt))
+        {
+            Toast.makeText(RegisterActivity.this, "All fields required!", Toast.LENGTH_SHORT).show();
+        }
+        else if(password_txt.length() < 6)
+        {
+            Toast.makeText(RegisterActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            register(username_txt, email_txt, password_txt);
+        }
+    }
+
+    private void register(final String username, String email, String password)
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        FirebaseUser firebaseUser = auth.getCurrentUser();
+                        assert firebaseUser != null;
+                        String userid = firebaseUser.getUid();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("id", userid);
+                        hashMap.put("username", username);
+
+                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>()
+                                    {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task)
+                                        {
+                                            if(task.isSuccessful())
+                                            {
+                                                Toast.makeText(RegisterActivity.this, "Registered successfully. Please check your email for verification", Toast.LENGTH_SHORT).show();
+
+                                            }
+                                            else
+                                            {
+                                                Toast.makeText(RegisterActivity.this, "Failed to send email confirmation!", Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+    }
+
 }
